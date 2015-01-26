e7851bc4b2ebe7181bedd5ee12b86b1cd1d31c9d
ohmyallah <ohmyallah@nate.com>
2015-01-26 17:53:01 +0900

    init test commit

---
+++ studiolib/src/main/java/com/example/h_dev1/studiolib/LibProjectMessage.java
@@ -0,0 +1,19 @@
+package com.example.h_dev1.studiolib;
+
+/**
+ * Created by H_DEV1 on 2015-01-26.
+ */
+public class LibProjectMessage {
+
+    private String msg;
+
+    public LibProjectMessage(String message) {
+
+        this.msg = message;
+    }
+
+    public String getMessage() {
+
+        return "Message from LibProject " + this.msg;
+    }
+}
